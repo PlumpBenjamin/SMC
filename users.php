62c3d901ff113de1164724a3fcd0264420419e31
PlumpBenjamin <873589@pdsb.net>
2020-02-13 18:42:35 +0000

    Tryna fix php code

--- users.php
+++ users.php
@@ -6,48 +6,46 @@ $password = $_POST['password'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 
-if (!empty($name) || !empty($username) || !empty($password) || !empty($email) || !empty($phone)); {
+if (!empty($name) || !empty($username) || !empty($password) || !empty($email) || !empty($phone)) {
 
-	$host = "localhost";
-	$dbUsername = "root";
-	$dbPassword = "";
-	$dbname = "pracdata";
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "pracdata";
 
-	// connecting
-	$conn = new mysql($host, $dbUsername, $dbPassword, $dbname);
 
-	if (mysql_connect_error()); {
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From users Where email = ? Limit 1";
+     $INSERT = "INSERT Into users (name, username, password, email, phone) values(?, ?, ?, ?)";
 
-		die('Connect Error('.mysqli_connect_errno().')'.mysql_connect_error());
-	} else {
-		$SELECT = "SELECT email From users Where email = ? Limit 1";
-		$INSERT = "INSERT Into users (name, username, password, email, phone) values(?,?,?,?,?)";
 
-		//Prep
-		$stmt = $conn->prepare($SELECT);
-		$stmt->blind_param("s", $email);
-		$stmt->execute();
-		$stmt->bind_result();
-		$stmt->store_result();
-		$rnum = $stmt->num_row;
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
 
-		if ($rnum == 0) {
-			$stmt->close();
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssssii", $username, $username, $password, $email, $phone);
 
-			$stmt = $conn->prepare($INSERT);
-			$stmt->bind_param("ssssii", $name, $username, $password, $email, $phone);
-			$stmt->execute();
-			echo "Registration Successful!";
-		} else {
-			echo "Someone is already registered with this email";
-		}
-		$stmt->close();
-		$conn->close();
-	}
-
+      $stmt->execute();
+      echo "Your account has been registered!";
+     } else {
+      echo "This email is already linked to Preak account";
+     }
+     $stmt->close();
+     $conn->close();
+    }
 } else {
-	echo "Please fill out all required areas.";
-	die();
+ echo "All fields are required";
+ die();
 }
-
 ?>
